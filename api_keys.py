8badf440d71e35d6d0fe772e647996a3eaffd3ee
StephenTrella <70353776+StephenTrella@users.noreply.github.com>
2020-10-19 17:36:53 -0700

    Add files via upload

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "514decfd53246b0714db38b1f73e7d1d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAxP3RYFDFHchnquM1ujEZHYDAd2qMvjqQ"
